5cacb197b86b71c73db84f24f7288daffb4cee98
Elie Obeid <elieobeid7@gmail.com>
2018-08-15 02:07:37 +0300

    finished subcategory page

--- src/pages/subcategories/subcategories.ts
+++ src/pages/subcategories/subcategories.ts
@@ -24,5 +24,22 @@ export class SubcategoriesPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad SubcategoriesPage');
   }
-
+  onLastsubCategory(category) {
+    // if category is an admin category, it means it's a wordrpress posts page
+    if (category.admin) {
+      this.navCtrl.push('PostsPage', { category: category });
+    }
+    // this is all stuff in a category
+    else if (category.main) {
+      this.navCtrl.push('AllCategoriesPage', { category: category });
+    }
+    // another sub category page
+    else if (category.sub && !category.main) {
+      this.navCtrl.push('LastsubcategoryPage', { category: category });
+    }
+    // items page
+    else if (!category.sub && !category.main && !category.admin) {
+      this.navCtrl.push('CategoryDetailsPage', { category: category });
+    }
+  }
 }
